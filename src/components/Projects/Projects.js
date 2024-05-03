493e8c5df9e3eec7cda48da8288ae9d5a92c23da
Aravind Manikanda Raja P <149185964+aravindamr21@users.noreply.github.com>
2024-05-03 12:25:01 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -96,9 +96,49 @@
 // }
 
 // export default Projects;
+// import React from "react";
+// import { Container, Row, Col } from "react-bootstrap";
+// import ProjectCard from "./ProjectCards";
+// import Particle from "../Particle";
+// import leaf from "../../Assets/Projects/leaf.png";
+// import emotion from "../../Assets/Projects/emotion.png";
+// import editor from "../../Assets/Projects/codeEditor.png";
+// import chatify from "../../Assets/Projects/chatify.png";
+// import suicide from "../../Assets/Projects/suicide.png";
+// import bitsOfCode from "../../Assets/Projects/blog.png";
+
+// function Projects() {
+//   return (
+//     <Container fluid className="project-section">
+//       <Particle />
+//       <Container>
+//         <h1 className="project-heading">
+//           My Recent <strong className="purple">Works </strong>
+//         </h1>
+//         {/* Project 1 */}
+//         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+//           <Col md={4} className="project-card">
+//             <ProjectCard
+//               imgPath={chatify}
+//               isBlog={false}
+//               title="Chatify"
+//               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+//               ghLink="https://github.com/soumyajit4419/Chatify"
+//               demoLink="https://chatify-49.web.app/"
+//             />
+//           </Col>
+//           {/* Include more project cards here */}
+//         </Row>
+//         {/* Project 2, 3, 4, 5, 6, etc. */}
+//       </Container>
+//     </Container>
+//   );
+// }
+
+// export default Projects;
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import ProjectCard from "./ProjectCards";
+import ProjectCards from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
@@ -115,10 +155,9 @@ function Projects() {
         <h1 className="project-heading">
           My Recent <strong className="purple">Works </strong>
         </h1>
-        {/* Project 1 */}
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
-            <ProjectCard
+            <ProjectCards
               imgPath={chatify}
               isBlog={false}
               title="Chatify"
@@ -127,9 +166,8 @@ function Projects() {
               demoLink="https://chatify-49.web.app/"
             />
           </Col>
-          {/* Include more project cards here */}
+          {/* Add more ProjectCards components for other projects */}
         </Row>
-        {/* Project 2, 3, 4, 5, 6, etc. */}
       </Container>
     </Container>
   );
